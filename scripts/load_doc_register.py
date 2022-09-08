50435ccc28782cd17364ae8d0d79e04786c6e08d
Alejandro Villar <avillar@ogc.org>
2022-09-07 20:03:51 -0400

    Test for adding JSON-LD context with github action

--- scripts/load_doc_register.py
+++ scripts/load_doc_register.py
@@ -1,26 +1,22 @@
 # from __future__ import print_function
 import json
-import requests
-import sys
-import urllib.request
-from typing import List, Any
+import logging
 import argparse
-import httpx
+import re
+from typing import Union, Optional, List
+
 import rdflib
-import os.path
 from rdflib import Graph
 from rdflib.namespace import DC, DCTERMS, SKOS, OWL, RDF, RDFS, XSD, DCAT
 from pyld import jsonld
 import jq
+from os import path
 from jsonpath_ng.ext import parse as jsonpathparse
-# load the document register as JSON via a JSON-LD conversion
-
-DOCSURL = "https://docs.google.com/spreadsheets/d/"
-# SAMPLE_RANGE_NAME = 'Class Data!A1:D'
 
 
 def init_graph() -> Graph:
     g = rdflib.Graph()
+    g.bind("dc", DC)
     g.bind("xsd", XSD)
     g.bind("dct", DCTERMS)
     g.bind("skos", SKOS)
@@ -36,7 +32,7 @@ def init_graph() -> Graph:
     return g
 
 
-def process_input(data, contextfn):
+def process_input(data: dict, contextfn: str):
     # Load YAML context file
     from yaml import load
     try:
@@ -71,7 +67,7 @@ def process_input(data, contextfn):
     return data
 
 
-def main(inputfn, outputfn, contextfn, base=None):
+def generate_graph(inputfn: str, contextfn: str, base: Optional[str] = None):
 
     g = init_graph()
 
@@ -80,51 +76,163 @@ def main(inputfn, outputfn, contextfn, base=None):
         inputdata = json.load(j)
 
     jdocld = process_input(inputdata, contextfn)
-    with open(f'{outputfn}.jsonld', 'w') as f:
-        json.dump(jdocld, f, indent=2)
 
     options = {}
     if base:
         options['base'] = base
-    output = json.dumps(jsonld.expand(jdocld, options))
+    output = json.dumps(jsonld.expand(jdocld, options), indent=2)
     g.parse(data=output, format='json-ld')
 
-    formatted_ttl: str = str(g.serialize(format="turtle"))
-    with open(outputfn, 'w') as fout_ttl:
-        fout_ttl.write(formatted_ttl)
-        fout_ttl.write('\n')
+    return g, output
+
+
+def process(inputfn: str,
+            jsonldfn: Optional[Union[bool, str]] = False,
+            ttlfn: Optional[Union[bool, str]] = False,
+            contextfn: Optional[str] = None,
+            base: Optional[str] = None,
+            skip_on_missing_context: bool = False) -> List[str]:
+
+    if not path.isfile(inputfn):
+        raise IOError(f'Input is not a file ({inputfn})')
+
+    inputbase, inputext = path.splitext(inputfn)
+
+    if not contextfn:
+        # Autodiscover context
+        for cfn in [
+            f'{inputfn}.yml',
+            f'{inputfn}.yaml',
+            f'{inputbase}.yaml',
+            f'{inputbase}.yml',
+        ]:
+            if path.isfile(cfn):
+                contextfn = cfn
+                break
+    if not contextfn:
+        if skip_on_missing_context:
+            logging.warning("No context file provided and one could not be discovered automatically. Skipping...")
+            return []
+        raise Exception('No context file provided and one could not be discovered automatically')
+
+    g, jsonlddoc = generate_graph(inputfn, contextfn, base)
+
+    createdfiles = []
+    # False = do not generate
+    # None = auto filename
+    # - = stdout
+    if ttlfn or ttlfn is None:
+        if ttlfn == '-':
+            print(g.serialize(format='ttl'))
+        else:
+            if not ttlfn:
+                ttlfn = f'{inputbase}.ttl' if inputext != '.ttl' else f'{inputfn}.ttl'
+            g.serialize(destination=ttlfn, format='ttl')
+            createdfiles.append(ttlfn)
+
+    # False = do not generate
+    # None = auto filename
+    # "-" = stdout
+    if jsonldfn or jsonldfn is None:
+        if jsonldfn == '-':
+            print(jsonlddoc)
+        else:
+            if not jsonldfn:
+                jsonldfn = f'{inputbase}.jsonld' if inputext != '.jsonld' else f'{inputfn}.jsonld'
+            with open(jsonldfn, 'w') as f:
+                f.write(jsonlddoc)
+            createdfiles.append(jsonldfn)
+
+    return createdfiles
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-i",
-        "--input",
+        "input",
         help="Source file (instead of service)",
-        default="../incubation/bibliography/test.jsonld"
     )
 
     parser.add_argument(
-        "-o",
-        "--output",
-        help="Output JSON-LD filename",
-        default='docs.ttl'
+        '-j',
+        '--json-ld',
+        action='store_true',
+        help="Generate JSON-LD output file",
+    )
+
+    parser.add_argument(
+        '--json-ld-file',
+        help='JSON-LD output filename',
+    )
+
+    parser.add_argument(
+        '-t',
+        '--ttl',
+        action='store_true',
+        help='Generate TTL output file',
+    )
+
+    parser.add_argument(
+        "--ttl-file",
+        help="TTL output filename",
     )
 
     parser.add_argument(
         '-c',
         '--context',
-        help='YAML context file',
-        default='../incubation/bibliography/context.yml'
+        help='YAML context file (instead of autodetection)',
     )
 
     parser.add_argument(
         '-b',
-        '--base',
+        '--base-uri',
         help='Base URI for JSON-LD',
-        default='http://example.org/vocab#'
+    )
+
+    parser.add_argument(
+        '-s',
+        '--skip-on-missing-context',
+        help='Skip files for which a context definition cannot be found (instead of failing)',
+    )
+
+    parser.add_argument(
+        '--batch',
+        help='Batch processing where input file is one or more files separated by commas, context files are '
+             'autodiscovered and output file names are always auto generated',
+        action='store_true'
+    )
+
+    parser.add_argument(
+        '--fs',
+        help='File separator for formatting list of output files (no output by default)',
     )
 
     args = parser.parse_args()
 
-    main(args.input, args.output, args.context, args.base)
+    outputfiles = []
+    if args.batch:
+        for fn in args.input.split(','):
+            if not re.match(r'.*\.json-?(ld)?$', fn):
+                continue
+            try:
+                outputfiles += process(
+                    fn,
+                    jsonldfn=None if args.json_ld else False,
+                    ttlfn=None if args.ttl else False,
+                    contextfn=None,
+                    base=args.base_uri,
+                    skip_on_missing_context=True
+                )
+            except Exception as e:
+                logging.warning("Error processing JSON/JSON-LD file, skipping: %s", str(e))
+    else:
+        outputfiles += process(args.input,
+            jsonldfn=args.json_ld_file if args.json_ld else False,
+            ttlfn=args.ttl_file if args.ttl else False,
+            contextfn=args.context,
+            base=args.base_uri,
+            skip_on_missing_context=args.skip_on_missing_context,
+        )
+
+    if args.fs:
+        print(args.fs.join(outputfiles))
